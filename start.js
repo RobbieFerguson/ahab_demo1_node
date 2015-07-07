cef7ad14b2623c06fbf2a01d6e73dc8fa442a943
Michael Barzizza <mbarzizza1@gmail.com>
2015-07-07 12:16:33 -0400

    updating docker file

--- start.js
+++ start.js
@@ -1,9 +1,8 @@
 var fs = require('fs')
 var request = require('request')
 
-
-request.get('http://www.myapifilms.com/imdb/inTheaters')
-.pipe(fs.createWriteStream('./ahab/output.json'))
-.on('finish',function(){
-  console.log('finished writing')
-})
+request.get('http://www.myapifilms.com/imdb/inTheaters', function (err, response) {
+  fs.writeFile(__dirname + '/ahab/output.json', response.body, function (err) {
+    fs.createReadStream(__dirname + '/ahab/output.json').pipe(process.stdout);
+  });
+});
