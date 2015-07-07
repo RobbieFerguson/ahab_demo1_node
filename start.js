2dcc3875490469bb4c5e0374dae4a01593dd04d5
Rachel Charow <rachel.charow@gmail.com>
2015-07-07 12:38:06 -0400

    fixing stuff

--- start.js
+++ start.js
@@ -1,9 +1,11 @@
 var fs = require('fs')
 var request = require('request')
+var path = require('path');
 
+console.log('testing!!!!');
 
 request.get('http://www.myapifilms.com/imdb/inTheaters')
-.pipe(fs.createWriteStream('./ahab/output.json'))
+.pipe(fs.createWriteStream(path.join(__dirname, './ahab/output.json')))
 .on('finish',function(){
   console.log('finished writing')
 })
