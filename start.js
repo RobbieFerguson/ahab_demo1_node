d894a96fa15a3741fed1572302897caa878efb3a
Rachel Charow <rachel.charow@gmail.com>
2015-07-07 19:02:26 -0400

    update test file

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ var request = require('request')
 var path = require('path');
 
 request.get('http://www.myapifilms.com/imdb/inTheaters', function (err, response) {
-  process.stdout("THIS IS THE DIRNAME IN THE CONTAINER", __dirname);
+  process.stdout.write("THIS IS THE DIRNAME IN THE CONTAINER", __dirname);
   fs.writeFile(__dirname + '/ahab/output.json', response.body, function (err) {
     fs.createReadStream(__dirname + '/ahab/output.json').pipe(process.stdout);
   });
